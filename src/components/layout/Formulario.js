2c0d58500c88de25e0649eee195a0b8660c0632c
olaldeJP <olaldejp@gmail.com>
2024-02-20 16:17:09 -0300

    update

--- src/components/layout/Formulario.js
+++ src/components/layout/Formulario.js
@@ -1,24 +1,30 @@
+import "../../style/components/layout/Formulario.css";
+
 const Formulario = (e) => {
   return (
-    <form>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-      </div>
-      <div>
-        <label htmlFor="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required />
-      </div>
-      <div>
-        <label htmlFor="telefono">Teléfono:</label>
-        <input type="tel" id="telefono" name="telefono" />
-      </div>
-      <div>
-        <label htmlFor="mensaje">Mensaje:</label>
-        <textarea id="mensaje" name="mensaje" required></textarea>
-      </div>
-      <button type="submit">Enviar</button>
-    </form>
+    <div className="divFormulario">
+      <form className="formulario" id="formulario">
+        <div className="divInfo">
+          <label htmlFor="email">* Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="divInfo">
+          <label htmlFor="nombre">Nombre: </label>
+          <input type="text" id="nombre" name="nombre" required />
+        </div>
+        <div className="divInfo">
+          <label htmlFor="telefono">Teléfono: </label>
+          <input type="tel" id="telefono" name="telefono" />
+        </div>
+        <div className="divInfo">
+          <label htmlFor="mensaje">* Mensaje: </label>
+          <textarea id="mensaje" name="mensaje" required></textarea>
+        </div>
+        <button type="submit" className="submitButton">
+          Enviar
+        </button>
+      </form>
+    </div>
   );
 };
 
--- src/components/layout/Inauguramos.js
+++ src/components/layout/Inauguramos.js
@@ -1,11 +1,8 @@
 import fondo from "../../img/fondo_main.jpg";
+import "../../style/components/layout/Inauguramos.css";
 
-
-const Inauguracion = (props) =>
-{
-return (
-    <img src={fondo} alt="fondo" />
-)
+const Inauguracion = (props) => {
+  return <img src={fondo} alt="fondo" className="fondoMain" />;
 };
 
-export default Inauguracion
+export default Inauguracion;
