ef2f589a23d7d985bd5d3dc3edfec791f6a9a54d
olaldeJP <olaldejp@gmail.com>
2024-05-07 12:29:39 -0300

    update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,33 @@ import Formulario from "./components/layout/Formulario";
 import Horarios from "./components/layout/Horarios";
 import Carrusel from "./components/layout/Carrusel";
 import LoginVar from "./components/layout/logginVar";
-
+import logeadoVar from "./components/layout/logeadoVar";
+import React, { useState } from "react";
 function App() {
+  const [userSession, setUserSession] = useState(null);
+
+  const sesion = peticionCurrent();
+  console.log(sesion.status);
+  if (sesion) {
+    return returnSinLoginVar();
+  } else return returnConLoginVar();
+  /*return (
+    <div className="App">
+      <Header></Header>
+      <Nav></Nav>
+      <LoginVar></LoginVar>
+      <Inauguracion></Inauguracion>
+      <Carrusel></Carrusel>
+      <Horarios></Horarios>
+      <Formulario></Formulario>
+      <Footer></Footer>
+    </div>
+  );*/
+}
+
+export default App;
+
+function returnSinLoginVar() {
   return (
     <div className="App">
       <Header></Header>
@@ -23,4 +48,35 @@ function App() {
   );
 }
 
-export default App;
+function returnConLoginVar() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Nav></Nav>
+      <logeadoVar></logeadoVar>
+      <Inauguracion></Inauguracion>
+      <Carrusel></Carrusel>
+      <Horarios></Horarios>
+      <Formulario></Formulario>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+async function peticionCurrent() {
+  const user = await fetch("http://localhost:8080/api/sesion/current", {
+    method: "GET",
+    credentials: "include",
+  })
+    .then((response) => {
+      console.log(response.status);
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("No session");
+    })
+    .catch(() => {
+      return "Error";
+    });
+  return user;
+}
---
+++ src/components/layout/logeadoVar.js
@@ -0,0 +1,8 @@
+const logeadoVar = (props) => {
+  return (
+    <div className="d-flex pding justify-content-between">
+      <h1>HOLA</h1>
+    </div>
+  );
+};
+export default logeadoVar;
